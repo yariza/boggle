716c6dc7a3c626c73c821023522aca4528feadf2
Yujin Ariza <yujin.ariza@gmail.com>
2017-02-23 14:19:22 -0500

    updated gtest url

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -7,7 +7,7 @@ include(ExternalProject)
 # Download and install GoogleTest
 ExternalProject_Add(
     gtest
-    URL https://googletest.googlecode.com/files/gtest-1.7.0.zip
+    URL https://github.com/google/googletest/archive/release-1.8.0.zip
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
     # Disable install step
     INSTALL_COMMAND ""
@@ -20,13 +20,13 @@ add_dependencies(libgtest gtest)
 # Set gtest properties
 ExternalProject_Get_Property(gtest source_dir binary_dir)
 set_target_properties(libgtest PROPERTIES
-    "IMPORTED_LOCATION" "${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a"
+    "IMPORTED_LOCATION" "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a"
     "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
 #    "INTERFACE_INCLUDE_DIRECTORIES" "${source_dir}/include"
 )
 # I couldn't make it work with INTERFACE_INCLUDE_DIRECTORIES
 # include_directories("${source_dir}/include")
 
-set(GTEST_INCLUDE_DIR ${source_dir}/include)
-set(GTEST_LIBRARY_PATH ${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
+set(GTEST_INCLUDE_DIR ${source_dir}/googletest/include)
+set(GTEST_LIBRARY_PATH ${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
 set(GTEST_LIBRARY libgtest)
